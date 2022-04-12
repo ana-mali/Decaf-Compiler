5281bc1ba47764b5c8795d939d49646730080cd4
ana-mali <92402688+ana-mali@users.noreply.github.com>
2022-04-11 21:11:55 -0400

    Update TokenClass.py

--- TokenClass.py
+++ TokenClass.py
@@ -11,12 +11,17 @@ from Elements import *
 import tokenize 
 class Token:
     def __init__(self,string,line_num,line):
+        """
+        Creates a Token object with string name of token
+        the line number in which it is used 
+        and the full string of the line it is on
+        """
         self.string=string #string of token
         self.line_num=line_num #line number in which it was found
         self.line=line #string of tokens line
 class symbol_object:
         """
-        functions for symbol object
+        function for symbol objects
         """
         def __init__(self,token_obj):
             """
@@ -24,11 +29,11 @@ class symbol_object:
                 token_obj - list -> token, type,attribute,value 
             """
 #            assert len(token_obj)==4,"invalid token"
-            self.token=token_obj[0]
-            self.type=token_obj[1]
-            self.attribute=token_obj[2]
-            self.value=token_obj[3]
-            self.line=[]
+            self.token=token_obj[0] #list
+            self.type=token_obj[1] #string
+            self.attribute=token_obj[2] #string
+            self.value=token_obj[3] #string 
+            self.line=[] #list
             self.line.append(token_obj[0].line_num)
 class Tokenizer:
     """
@@ -44,8 +49,9 @@ class Tokenizer:
         self.num_error=0
     def tokenize(self):
         """
-        Returns:
-         a list of symbol table values
+        -reads filename and creates token objects
+        -token objects are turned to symbol objects after analysis
+        -symbol objects get added to symbol table that can be printed after
         """
         buff1=[]
         buff2=[]
@@ -69,31 +75,35 @@ class Tokenizer:
                         if (len(buff2)>=MAXARRAYSIZE):# if buff2 is full and buff1 is not..
                             buff2.clear()# then clear buff2
                         buff1.append(char)
+                    if (len(buff1)<2):
+                        continue;
                     if (start[1]==1 and curr[1]==1): #in buffer 1
                         if (buff1[curr[0]+1].isspace()):#if lookahead is whitespace
-                            a=Token(buff1[start[0]:curr[0]],lines,line)
+                            a=Token(''.join(buff1[start[0]:curr[0]+1]),lines,line)
                             if (special_characters[2] in a.line):
-                                start=curr
+                                index=curr[0]
+                                buff=curr[1]
+                                start[0]=index 
+                                start[1]=buff
                                 curr[0]+=1
                                 continue;
                             if (a.string in keywords):
                                 token_obj=[a,None,'keyword',None] 
                             elif(a.string in operators):
                                 token_obj=[a,None,'operator',None]
-                            else: 
+                            elif (a.string not in special_characters):
                                 if (not a.string[0].isalpha()):
-                                    self.error(a,self.error_file,'Incorrect identifier name')
+                                    self.error(a,'Incorrect identifier name')
                                 else:
                                     token_obj=self.is_identifier(a)
-                        if (len(buff1)==MAXARRAYSIZE-1):
-                            curr=[0,2]#change current to next buffer if buff1 is full
-                        if (len(buff1)==MAXARRAYSIZE-1):
-                            start=[0,2]
                     elif (start[1]==2 and curr[1]==2): #in buffer 2
                         if (buff2[curr[0]+1].isspace()):#if lookahead is whitespace
-                            a=Token(buff2[start[0]:curr[0]],lines,line)
+                            a=Token(''.join(buff2[start[0]:curr[0]+1]),lines,line)
                             if (special_characters[2] in a.line):
-                                start=curr
+                                index=curr[0]
+                                buff=curr[1]
+                                start[0]=index 
+                                start[1]=buff
                                 curr[0]+=1
                                 continue;
                             if (a.string in keywords):
@@ -102,19 +112,22 @@ class Tokenizer:
                                 token_obj=[a,None,'operator',None]
                             else:
                                 if (not a.string[0].isalpha()):
-                                    self.error(a,self.error_file,'Incorrect identifier name')
+                                    self.error(a,'Incorrect identifier name')
                                 else:
-                                    token_obj=self.is_identifier(a)
+                                    if (not a.string in special_characters):
+                                        token_obj=self.is_identifier(a)
                         if (curr[0]==MAXARRAYSIZE-1): #end of buffer
                             curr=[0,2]#change current to next buffer if buff1 is full
                         if (start[0]==MAXARRAYSIZE-1):
                             start=[0,2]
                     elif (start[1]==1 and curr[1]==2):#start in buff1, curr in buff2
                         if (buff2[curr[0]+1].isspace()):
-                            a=Token(buff1[start[1:-1]]+buff2[curr[0:1]],lines,line)
+                            a=Token(''.join(buff1[start[1:-1]]+buff2[0:curr[0]+1]),lines,line)
                             if (special_characters[2] in a.line):
-                                start=curr
-                                curr[0]+=1
+                                index=curr[0]
+                                buff=curr[1]
+                                start[0]=index 
+                                start[1]=buff
                                 continue;
                             if (a.string in keywords):
                                 token_obj=[a,None,'keyword',None]
@@ -122,14 +135,18 @@ class Tokenizer:
                                 token_obj=[a,None,'operator',None]
                             else:
                                 if (not a.string[0].isalpha()):
-                                    self.error(a,self.error_file,'Incorrect identifier name')
+                                    self.error(a,'Incorrect identifier name')
                                 else:
-                                    token_obj=self.is_identifier(a)
+                                    if (not a.string in special_characters):
+                                        token_obj=self.is_identifier(a)
                     elif (start[1]==2 and curr[1]==1): #start in buff2, curr in buff1
                         if (buff1[curr[0]+1].isspace()):
-                            a=Token(buff2[start[1]:-1]+buff1[0:curr[1]],lines,line)
+                            a=Token(''.join(buff2[start[1]:-1]+buff1[0:curr[1]+1]),lines,line)
                             if (special_characters[2] in a.line):
-                                start=curr
+                                index=curr[0]
+                                buff=curr[1]
+                                start[0]=index 
+                                start[1]=buff
                                 curr[0]+=1
                                 continue;
                             if (a.string in keywords):
@@ -138,14 +155,22 @@ class Tokenizer:
                                 token_obj=[a,None,'operator',None]
                             elif (a.string not in special_characters):#ignore any special characters
                                 if (not a.string[0].isalpha()):
-                                    self.error(a,self.error_file,'Incorrect identifier name')
+                                    self.error(a,'Incorrect identifier name')
                                 else:
-                                    token_obj=self.is_identifier(a)
+                                    if (not a.string in special_characters):
+                                        token_obj=self.is_identifier(a)
                     if (token_obj!=None):
                         b=symbol_object(token_obj)
                         self.table.append(b)
-                        start=curr 
-                        
+                        index=curr[0]
+                        buff=curr[1]
+                        start[0]=index+2 #skip white space and last letter of token
+                        start[1]=buff 
+                        print(b.token.string)
+                        if (curr[0]+1<MAXARRAYSIZE):
+                            curr[0]+=1
+                    else:
+                        curr[0]+=1
         return
     def is_identifier(self,token):
         for x in self.table:
@@ -180,9 +205,9 @@ class Tokenizer:
             token_obj.append(keywords[7]) #attribute
             token_obj.append(None) #value
         return token_obj
-    def error(self,token,f,s):
-        fh=open(f,'r+')
-        print("Error on line: "+str(token.start[0])+'; '+s,file=fh)
+    def error(self,token,s):
+        fh=open(self.error_file,'r+')
+        print("Error on line: "+str(token.line_num)+'; '+s,file=fh)
         print('\n',file=fh)
         fh.close()
         self.num_error+=1
@@ -194,13 +219,7 @@ class Tokenizer:
         Returns:
             a printed table to a file 
         """
-        fh=open(output_filename,'w+')
-#        token_str_list=[]
-        with tokenize.open(self.filename) as f:
-            tokens = tokenize.generate_tokens(f.readline)
-            for token in tokens:
-                print(token,file=fh)
-#                fh.write('\n'.join('{}[}'.format(t[0], t[1]) for t in token_str_list))
-        
-        print('number of identifiers: {}'.format(self.num_id),file=fh)
-        fh.close()
+        for x in self.table:
+            print(x)
+        print('number of identifiers: {}'.format(self.num_id))
+    return
