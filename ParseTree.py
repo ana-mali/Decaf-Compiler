4d1ff8d6bc6b13f722d5c962113a73cc73ad8e18
ana-mali <92402688+ana-mali@users.noreply.github.com>
2022-04-16 18:34:21 -0400

    Update ParseTree.py

--- ParseTree.py
+++ ParseTree.py
@@ -30,8 +30,11 @@ class Node:
             
         '''
         self.tokens=[]
-        for x in symbol_objects:
-            self.tokens.append(x)
+        if (type(symbol_objects)==list):
+            for x in symbol_objects:
+                self.tokens.append(x)
+        elif(type(symbol_objects)==symbol_object):
+            self.tokens.append(symbol_objects)
         self.states=self.generate_states(counter)
         self.right=None 
         self.left=None
@@ -43,7 +46,7 @@ class Node:
         parsing tree to check grammer
         '''
         if (counter==0):
-            state0=[[state_machine[1],keywords[11]],[state_machine[2],'global'],
+            state0=[[state_machine[1],keywords[11]],['package','global'],
                 [state_machine[4],operators[0]],[state_machine[1],keywords[15]],
                 [state_machine[2],'global'],[state_machine[1],keywords[9]],
                 [state_machine[4],operators[8]],[state_machine[3],'number'],
@@ -53,7 +56,7 @@ class Node:
                 [state_machine[4],operators[5]]]
             return state0
         elif (counter==1):
-            state1=[[state_machine[1],keywords[7]],[state_machine[2],'GreatestCommonDivisor'],
+            state1=[[state_machine[1],keywords[7]],['func','GreatestCommonDivisor'],
                 [state_machine[4],operators[6]],[state_machine[4],operators[7]],
                 [state_machine[1],keywords[9]],[state_machine[4],operators[0]],
                 [state_machine[1],keywords[15]],[state_machine[2],'main'],
@@ -65,18 +68,18 @@ class Node:
                 [state_machine[2],'main'],[state_machine[4],operators[8]],
                 [state_machine[2],'GreatestCommonDivisor'],[state_machine[4],operators[5]],
                 [state_machine[2],'main'],[state_machine[4],operators[8]],
-                [state_machine[2],'GreatestCommonDivisor'],[state_machine[4],operators[6]],
+                ['func','GreatestCommonDivisor'],[state_machine[4],operators[6]],
                 [state_machine[2],'main'],[state_machine[4],operators[4]],
                 [state_machine[2],'main'],[state_machine[4],operators[7]],
                 [state_machine[4],operators[5]],[state_machine[4],operators[1]]]
             return state1
         elif (counter==2):
-            state2=[[state_machine[1],keywords[7]],[state_machine[2],'GreatestCommonDivisor'],
+            state2=[[state_machine[1],keywords[7]],['func','GreatestCommonDivisor'],
                 [state_machine[4],operators[6]],[state_machine[2],'GreatestCommonDivisor'],
                 [state_machine[1],keywords[9]],[state_machine[4],operators[4]],
                 [state_machine[2],'GreatestCommonDivisor'],[state_machine[1],keywords[9]],
                 [state_machine[4],operators[7]],[state_machine[1],keywords[9]],
-                [state_machine[4],operators[5]]]
+                [state_machine[4],operators[0]]]
             return state2
         elif (counter==3):
             state3=[[state_machine[1],keywords[8]],[state_machine[4],operators[6]],
@@ -90,9 +93,14 @@ class Node:
         elif (counter==4):
             state4=[[state_machine[1],keywords[3]],[state_machine[4],operators[0]],
                 [state_machine[1],keywords[12]],[state_machine[4],operators[6]],
-                [state_machine[2],'gcd'],[state_machine[4],operators[4]],
-                [state_machine[2],'GreatestCommonDivisor'],[state_machine[4],operators[18]],
-                [state_machine[2],'GreatestCommonDivisor'],[state_machine[4],operators[7]],
+                ['func','GreatestCommonDivisor'],#gcd,
+                [state_machine[4],operators[6]],
+                [state_machine[2],'GreatestCommonDivisor'],#b
+                [state_machine[4],operators[4]],
+                [state_machine[2],'GreatestCommonDivisor'],#a
+                [state_machine[4],operators[18]],#modulus
+                [state_machine[2],'GreatestCommonDivisor'],#b
+                [state_machine[4],operators[7]],
                 [state_machine[4],operators[7]],[state_machine[4],operators[5]],
                 [state_machine[4],operators[1]]]
             return state4
@@ -126,6 +134,8 @@ class Parse_tree:
         self.error_file=error_filename
         self.head=None
         self.tokenizer=tokenizer
+        self.count=0
+        self.error_lines=[]
 #            self.previousscope=None
     def Create_ParseTree(self):
         '''
@@ -141,6 +151,7 @@ class Parse_tree:
         index_curr=0
         num_open=0 #open curly brackets
         num_close=0 #closed curly brackets
+        check=None
         for x in symbol_table:
             if (x==symbol_table[0]):
                 continue
@@ -148,23 +159,42 @@ class Parse_tree:
             if(counter>6):
                 print('counter over ')
                 break;
-            if (index_curr>=len(symbol_table)):
-                self.check_section(symbol_table[index_start],counter)
+            if (index_curr>=len(symbol_table)-1):
+                section=symbol_table[index_start]
                 a=Node(section,counter)
                 self.insert_node(a, counter)
+                self.check_section(a)
                 counter+=1
+                section=symbol_table[index_curr]
+                a=Node(section,counter)
+                self.insert_node(a,counter)
+                self.check_section(a)
                 #call final section
             if (x.token.string==operators[0]):
                 num_open+=1
             elif (x.token.string==operators[1]):
+                if (check!=None):
+                    section=symbol_table[index_start:index_curr]
+                    a=Node(section,counter)
+                    self.insert_node(a, counter)
+                    self.check_section(a)
+                    counter+=1
+                    temp=index_curr 
+                    index_start=temp 
+                    check=None
+                else:
+                    check='close'
                 num_close+=1
+                '''
                 section=symbol_table[index_start:index_curr+1] #check lookahead
                 a=Node(section,counter)
                 self.insert_node(a,counter)
+                self.check_section(a)
                 index_curr+=1
                 temp=index_curr 
                 index_start=temp
                 counter+=1
+                '''
                 continue
             if (x.token.string==keywords[7] #func
                 or x.token.string==keywords[11] #package
@@ -173,9 +203,14 @@ class Parse_tree:
                 section=symbol_table[index_start:index_curr]
                 a=Node(section,counter)
                 self.insert_node(a,counter)
+                self.check_section(a)
                 temp=index_curr 
                 index_start=temp 
                 counter+=1
+                check=None
+        if (num_close!=num_open):
+            self.error(None, 'missing { or } bracket')
+        
     def insert_node(self,node,counter):
         if (self.head==None):
             self.head=node
@@ -189,10 +224,10 @@ class Parse_tree:
                 root.right.left=node
             elif (root.right.right==None and counter==4):
                 root.right.right=node 
-            elif (root.right.left==None and 
+            elif (root.right.left.right==None and 
                   root.right.right.left==None and 
                   counter==5):                
-                root.right.left=node
+                root.right.left.right=node
                 root.right.right.left=node
             elif (root.left.right==None and 
                   root.right.right.left.left==None and 
@@ -203,6 +238,7 @@ class Parse_tree:
                 root.right.left.right.left=node 
             else:
                 print('insert node incorrect')
+        self.count+=1
         return
     def check_section(self,node):      
         '''
@@ -220,22 +256,23 @@ class Parse_tree:
                 elif (node.tokens[x].attribute==state_machine[3]):#number
                     if (not node.tokens[x].token.string.isdigit()):
                         self.error(node.tokens[x],'Expected a number')
-                else:#identifier
-                    if (keywords[7] in node.tokens[x].token.line):#func 
-                        if(node.tokens[x].attribute!=keywords[7]):
-                            self.error(node.tokens[x],'Identifier must be a func')
-                    elif(keywords[11] in node.tokens[x].token.line):
-                        if (node.tokens[x].attribute!=keywords[11]):
-                            self.error(node.tokens[x],'Identifier must be a package')
-                    else:
-                        if (node.tokens[x].attribute!='id'):
-                            self.error(node.tokens[x],'Expected an identifier')
+                elif(node.tokens[x].attribute=='func'):
+                    continue
+                elif (node.tokens[x].attribute=='package'):
+                    continue
+                else:
+                    if (node.tokens[x].attribute!='id'):
+                        self.error(node.tokens[x],'Expected an identifier')
+            else:
+                self.error(node.tokens[x],'Incorrect attribute')
     def error(self,sym_obj,s):
         fh=open(self.error_file,'r+')
-        print("Error on line: "+str(sym_obj.token.line_num)+"; token: "+sym_obj.token.string+' ; '+s,file=fh)
+        if (sym_obj==None):
+            print(s)
+        else:
+            print("Error on line: "+str(sym_obj.token.line_num)+": token: "+sym_obj.token.string+' : '+s,file=fh)
         print('\n',file=fh)
         fh.close()
         self.tokenizer.num_error+=1
+        self.error_lines.append(sym_obj.token.line_num)
         return 
-        
-        
