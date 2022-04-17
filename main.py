14eb08a283eb41a4bffbd0be26e550b027ac7934
ana-mali <92402688+ana-mali@users.noreply.github.com>
2022-04-16 23:18:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ __updated__= "2022-04-15"
 '''
 from TokenClass import Tokenizer
 from ParseTree import *
+from Semantics import *
 a=Tokenizer('DexafCodeExample.txt','error.txt')#create token object...
 #with source filename and error filename
 
 #Lexical Analysis
 a.tokenize() #initiate tokenization
 
+for x in a.table:
+    print(x.token.string+' : '+x.attribute)
+a.count_identifiers()
+print('number of identifiers: '+ str(a.num_id))
+
 #Syntax Analysis 
 b=Parse_tree(a,'error.txt') #with tokenizer and error filename
 b.Create_ParseTree()
+
 #Semantic Analysis
+c=Semantic_analysis(a,b,'error.txt')
+c.Semantics()
+print('number of errors: '+ str(a.num_error))
 
 #a.print_table('output.txt') #print symbol table to output file
